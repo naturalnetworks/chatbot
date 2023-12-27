71a7cd0dde0024d19cff5f61d6446c1c67a4bfcc
Ben Johns <bjohns@naturalnetworks.net>
2023-12-27 15:51:05 +1000

    Added WeatherFlow function - returns weather data Added function to adapt markdown to mrkdwn (Slack)

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import functions_framework
 
 import google.generativeai as genai
 
-import mistune
+import re # need regular expressions to adapt markdown to mrkdwn...
+
+import requests # need this to interrogate WeatherFlow API
 
 logging.basicConfig(
     level=os.getenv("LOG_LEVEL", "DEBUG"), format="%(levelname)s: %(message)s"
@@ -28,15 +30,22 @@ app = App(
 )
 handler = SlackRequestHandler(app)
 
-class SlackMarkdownRenderer(mistune.Renderer):
-    def block_code(self, code, lang):
-        return f"`{lang}\n{code}\n`"
-    # ... (add other methods to handle additional markdown elements as needed)
+def adjust_markdown_for_slack(markdown_text):
+    # Change double asterisks to single asterisks for emphasis
+    markdown_text = re.sub(r'\*\*(.*?)\*\*', r'*\1*', markdown_text)
 
-def convert_to_slack_markdown(input_markdown):
-    markdown = mistune.Markdown(renderer=SlackMarkdownRenderer())  # Instantiate Markdown object with renderer
-    slack_markdown = markdown(input_markdown)  # Use markdown object to convert input text
-    return slack_markdown
+    # Add a newline after list items
+    # markdown_text = re.sub(r'(\d+\.)', r'\1\n', markdown_text)
+
+    # Convert headers to Slack's mrkdwn format
+    markdown_text = re.sub(r'^# (.+)$', r'*\1*', markdown_text, flags=re.MULTILINE)
+    markdown_text = re.sub(r'^## (.+)$', r'**\1**', markdown_text, flags=re.MULTILINE)
+    markdown_text = re.sub(r'^### (.+)$', r'***\1***', markdown_text, flags=re.MULTILINE)
+
+    # Use a monospaced font for better table representation
+    markdown_text = markdown_text.replace('|', '`|`')
+
+    return markdown_text
 
 def get_user_info(user_id):
     """
@@ -71,7 +80,8 @@ def query_ai(respond, query):
                 temperature=0.1)
             )
         # return ai_query_response.text
-        return convert_to_slack_markdown(ai_query_response.text)
+        return adjust_markdown_for_slack(ai_query_response.text)
+       
     except Exception as e:
         logging.error(f"query_ai returned: {e}")
         return "Error generating AI response. Please try again later."
@@ -136,6 +146,80 @@ def bard_command(ack, respond, command):
         "text": f"{username} asked \"{query}\".\nGenerated response:\n{ai_response}",  # Fallback text
         "blocks": response_blocks
     })
+
+@app.command("/wf")
+def get_tempest_weather_mrkdwn(ack, respond):
+
+    api_key = os.environ.get("WF_API_KEY")
+    station_id = os.environ.get("WF_STATION_ID")
+
+    url = f"https://api.tempestwx.com/api/v1/devices/{station_id}/observations/latest"
+    headers = {"Authorization": f"Bearer {api_key}"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        # Extract relevant weather data points
+        temperature = data["obs"][0]["air_temperature"]
+        feels_like = data["obs"][0]["feels_like"]
+        humidity = data["obs"][0]["relative_humidity"]
+        wind_gust = data["obs"][0]["wind_gust"]
+        wind_direction = data["obs"][0]["wind_direction"]
+        pressure = data["obs"][0]["sea_level_pressure"]
+        rain_rate = data["obs"][0]["precip_accum_last_1hr"]
+        rain_accumlated = data["obs"][0]["precip"]
+        solar_radiation = data["obs"][0]["solar_radiation"]
+        uv = data["obs"][0]["uv"]
+        lightning_strike_distance = data["obs"][0]["lightning_strike_last_distance"]
+
+        # Convert wind direction from degrees to cardinal direction
+        if wind_direction < 22.5:
+            wind_direction = "N"
+        elif wind_direction < 67.5:
+            wind_direction = "NE"
+        elif wind_direction < 112.5:
+            wind_direction = "E"
+        elif wind_direction < 157.5:
+            wind_direction = "SE"
+        elif wind_direction < 202.5:
+            wind_direction = "S"
+        elif wind_direction < 247.5:
+            wind_direction = "SW"
+        elif wind_direction < 292.5:
+            wind_direction = "W"
+        elif wind_direction < 337.5:
+            wind_direction = "NW"
+        else:
+            wind_direction = "N"
+
+        # Format as mrkdwn
+        wf_mrkdwn = f"""
+*Ben's Tempest Weather Report*
+*Temperature/Feels Like:* {temperature}°C/{feels_like}°C | *Humidity:* {humidity}% | *Wind Gust/Direction:* {wind_gust}km/h - {wind_direction} | *Pressure:* {pressure} mb | *Rain Rate/Accumlated:* {rain_rate}mm/hr/{rain_accumlated}mm | *Lightning Strike Distance:* {lightning_strike_distance}km | *Solar Radiation:* {solar_radiation}W/m^2 | *UV Index:* {uv})
+        """
+
+        response_blocks = [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"{wf_mrkdwn}"
+                }
+            }
+        ]
+       
+        # Use the app's respond method to send the response with blocks and fallback text
+        respond({
+            "response_type": "in_channel",
+            "text": f"{wf_mrkdwn}",  # Fallback text
+            "blocks": response_blocks
+        })
+    else:
+        respond({
+            "response_type": "in_channel",
+            "text": "Could not retrieve weather data."
+        })
 
 # Entry point function for Functions Framework
 @functions_framework.http
